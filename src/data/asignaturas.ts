cef2511cda8e179b4e390f2066279986fc067537
SugusGamberra <125316695+SugusGamberra@users.noreply.github.com>
2026-02-18 14:37:56 +0100

    Asignaturas de 2 e Imagenes de las mismas subidas

--- src/data/asignaturas.ts
+++ src/data/asignaturas.ts
@@ -7,10 +7,25 @@ interface Asignatura {
 // bbdd local
 export const ASIGNATURAS_DB: Record<string, Asignatura[]> = {
     '2daw-2026': [
-        // Poner en caso d q m lo pasen
+        { id: 'cliente', nombre: 'Desarrollo Web Entorno Cliente'},
+        { id: 'servidor', nombre: 'Desarrollo Web Entorno Servidor'},
+        { id: 'despliegue', nombre: 'Despliegue de Aplicaciones Web'},
+        { id: 'interfaces', nombre: 'Diseño de Interfaces Web'},
+        { id: 'sostenibilidad', nombre: 'Sostenibilidad'},
+        { id: 'digitalizacion', nombre: 'Digitalización'},
+        { id: 'ingles', nombre: 'Inglés Técnico'},
+        { id: 'empleabilidadII', nombre: 'Itinerario Personal Empleabilidad II'}
     ],
     '2dam-2026': [
-        // Poner en caso d q m lo pasen
+        { id: 'datos', nombre: 'Acceso a Datos'},
+        { id: 'di', nombre: 'Desarrollo de Interfaces'},
+        { id: 'servproc', nombre: 'Prog. Servicios y Procesos'},
+        { id: 'multdisp', nombre: 'Prog. Multimedia y Dispositivos'},
+        { id: 'gestion', nombre: 'Sistemas de Gestión Empresarial'},
+        { id: 'sost', nombre: 'Sostenibilidad'},
+        { id: 'digt', nombre: 'Digitalización'},
+        { id: 'ing', nombre: 'Inglés'},
+        { id: 'ipe', nombre: 'Itinerario Personal Empleabilidad II'}
     ],
     '1dam-2026': [
         { id: 'marcas', nombre: 'Lenguaje de Marcados' },
