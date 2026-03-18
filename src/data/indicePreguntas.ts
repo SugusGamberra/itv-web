925e4f4deb6699149a6b891715e0e73de64da9d8
SugusGamberra <125316695+SugusGamberra@users.noreply.github.com>
2026-03-18 14:41:17 +0100

    hotfix: import de las preguntas bbdd xd

--- src/data/indicePreguntas.ts
+++ src/data/indicePreguntas.ts
@@ -4,10 +4,11 @@ import type { Pregunta } from '../types/index';
 import lenguajeMarcas from './preguntas/lenguaje-marcas.json';
 import empleabilidad from './preguntas/empleabilidad.json';
 import servproc from './preguntas/servproc.json';
+import bbdd from './preguntas/bbdd.json';
 
 // AÑO 2025:
 import entornos from './preguntas/entornos-2025.json';
-import sistemas from './preguntas/sistemas-2025.json'
+import sistemas from './preguntas/sistemas-2025.json';
 
 // importar el resto d json cuando los tenga
 
@@ -16,6 +17,7 @@ export const MAPA_PREGUNTAS: Record<string, Pregunta[]> = {
     'marcas': lenguajeMarcas as Pregunta[],
     'empleabilidad': empleabilidad as Pregunta[],
     'servproc' : servproc as Pregunta[],
+    'bbdd' : bbdd as Pregunta[],
   // importar el resto segun lleguen aqui
 
   //2025
