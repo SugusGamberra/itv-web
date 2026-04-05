96ce94b6dbbdb44b38613c0142de6fb67b98ed24
SugusGamberra <125316695+SugusGamberra@users.noreply.github.com>
2026-04-05 18:13:54 +0200

    hf: error en la logica de shuffle, solucionado

--- src/app/hooks/useTestEngine.ts
+++ src/app/hooks/useTestEngine.ts
@@ -9,7 +9,12 @@ export const useTestEngine = (todasLasPreguntas: Pregunta[], modo: ModoTest) => 
 
     // barajar
     const shuffle = <T,>(array: T[]): T[] => {
-        return [...array].sort(() => Math.random() - 0.5);
+        const result = [...array];
+        for (let i = result.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [result[i], result[j]] = [result[j], result[i]]; // intercambio posiciiones x bug q se repiten 30 preguntas en entorons
+        }
+        return result;
     };
 
     // preparacion preguntas
